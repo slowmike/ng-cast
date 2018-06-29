db7a7cd72bac868881188c24330f437091060879
slowmike <aznwizard123@gmail.com>
2018-06-29 15:45:39 -0700

    Feed info from exampleVideoData to videoList

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,7 @@
 angular.module('video-player')
-
-.component('app', {
-  // TODO
-});
+  .component('app', {
+    templateUrl: 'src/templates/app.html',
+    controller: function() {
+      this.videos = window.exampleVideoData;
+    }
+  });
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,4 @@
 angular.module('video-player')
-
-.component('videoPlayer', {
-  // TODO
-});
+  .component('videoPlayer', {
+    templateUrl: 'src/templates/videoPlayer.html'
+  });
